302e47c342619ec1e442e2171131c7a5d4722b37
Johannes Heuel <johannes.heuel@rwth-aachen.de>
2024-02-08 17:56:32 +0100

    .

--- submitters/snakemake/zoidberg/grid-submit.py
+++ submitters/snakemake/zoidberg/grid-submit.py
@@ -4,12 +4,21 @@ import sys
 import requests
 from os import environ
 
+from snakemake.utils import read_job_properties
+
 jobscript = sys.argv[1]
+job_properties = read_job_properties(jobscript)
+
+payload = {
+    "cmd": jobscript
+}
+
+payload["threads"] = job_properties.get("threads", 1)
 
 resp = requests.post(
     "http://localhost:8080/submit",
     json=[
-        {"cmd": jobscript},
+        payload,
     ],
     headers={"cookie": environ["ZOIDBERG_SECRET"]},
 )
